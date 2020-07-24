96dd46eddbe5c035d913a8b5d0bfe6a966d3a7ec
Matheus Valeriano <math.valeriano@gmail.com>
2020-07-24 17:57:44 -0300

    feat: create breed type

---
+++ src/shared/types/breeds.ts
@@ -0,0 +1,10 @@
+export type Breed = {
+  readonly affection_level: number,
+  readonly description: string,
+  readonly energy_level: number,
+  readonly id: number,
+  readonly name: string,
+  readonly origin: string,
+  readonly temperament: string,
+  readonly wikipedia_url: string
+}
